243a6317a3fedeb6c9f2632daf8e44974a48a6b8
danharjor <daniel.harley.jordan@outlook.com>
2024-07-30 17:04:52 +0300

    I added in the save checkpoint property so that continuing to increase precision is possible

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ class Config():
         #The parsers main function is write_input_file
         # wite_input_file takes a parameters file from base_params_path and a set of points in the form of a dict {param1:[point1,point2...], param2:[point1,point2...]...} 
         #  It will then create a parameters file that can scan over all the points.
-        self.base_params_path = "/home/djdaniel/GENE_UQ/parameters_base_uq" 
+        self.base_params_path = "/home/djdaniel/GENE_UQ/parameters_base_uq_highprec" 
         self.remote_save_base_dir=f'/scratch/project_462000451/gene_out/gene_auto/'
         self.save_dir = "/home/djdaniel/GENE_UQ/temp/"
 
@@ -13,10 +13,11 @@ class Config():
         #The Runner is responsible for actually running a parameters file on lumi. Its main function is code_run.
         # code_run will take the set of points named samples and parse them into a parameters file.
         #  It then uses ssh to run GENE with this parametres file and a passed sbatch script.
-        self.host = 'lumi3' #needs to be configured in /home/<user>/.ssh/config
+        self.host = 'lumi1' #needs to be configured in /home/<user>/.ssh/config
         self.sbatch_base_path = "/home/djdaniel/GENE_UQ/sbatch_base" 
         # guess_sample_wallseconds = 200 # a guess for the number of seconds it takes to run one sample.
         self.remote_run_dir = '/project/project_462000451/gene/'
+        self.local_run_files_dir = "/home/djdaniel/GENE_UQ/run_files"
 
 config = Config()
 
